495307b5578adf008d33d30140b30955f7d76de2
gladwin90 <68490658+gladwin90@users.noreply.github.com>
2024-01-09 15:44:38 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  profile    = "${var.profile}"
+  region     = "${var.region}"
+  
+}
+/*
+resource "aws_s3_bucket" "example" {
+  bucket = "san-bucket63838146"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_dynamodb_table" "example_1" {
+  name         = "san-table"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+*/
+terraform {
+  backend "s3" {
+    bucket         = "san-bucket63838146"
+    dynamodb_table = "san-table"
+    key            = "global/mystatefile/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "Cluster_name" {
+  description = "Cluster Name"
+  type = string
+  default = "san-cluster1"
+}
+
+variable "VPC_name" {
+  description = "VPC Name"
+  type = string
+  default = "VPC_saneks"
+  
+}
+
+variable "profile" {
+    description = "AWS credentials profile you want to use"
+}
